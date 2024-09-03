199c4683970c9a30665da6c673d62d3c568de179
MildCode <161015409+MildCode@users.noreply.github.com>
2024-09-03 13:30:10 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the close button
+var span = document.getElementsByClassName("close")[0];
+
+// Open the modal
+function openModal() {
+    modal.style.display = "flex";
+}
+
+// Close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Close the modal when clicking outside of it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Example to open the modal on a button click (modify as needed)
+document.querySelector('.btn-primary').addEventListener('click', openModal);
